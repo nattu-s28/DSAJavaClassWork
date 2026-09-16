1ffcbaf63237dbe2f99e1b08ec558be714e190b4
Natarajan <vnadarajan15@gmail.com>
2026-09-16 15:51:12 +0530

    added Ques1 Sep 16

---
+++ Sep16/Ques1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ques1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            int size = sc.nextInt();
+            int arr[] = new int[size]; 
+            for (int i = 0; i < size; i++) {
+                arr[i] = sc.nextInt();
+            }
+            int Max = Integer.MIN_VALUE;
+            for (int i = 0; i < size; i++) {
+                Max = Math.max(Max,arr[i]);
+                System.out.print(Max + " ");
+            }
+        }
+        catch (Exception e){
+            System.out.println("Invalid Input");
+        }
+    }
+}
